9f0f4c67f0280a210c9f42207b082d3c7071d957
Karan Warrier <kawarrier@yahoo.com>
2020-05-24 15:29:54 -0700

    set up an app.post route

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,11 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
 messages = [{text:'lala', owner: 'Mola'}, {text: 'bosa', owner: 'meeshu'}];
 
+// need this to make sense of the body of requests being Posted
+app.use(bodyParser.json());
 
 app.use((req, res, next) => {
 	res.header("Access-Control-Allow-Origin", "*");
@@ -11,6 +15,12 @@ app.use((req, res, next) => {
 
 app.get('/messages', (req, res) => {
 	res.send(messages);
+    });
+
+app.post('/message', (req, res) => {
+        console.log(req.body);
+	// need to add a send status back or the Rest request will just hang forever
+	res.sendStatus(200);
     });
 
 app.listen(1234);
