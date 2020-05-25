96abc420586912c62f2f6290e6af1d5891016d23
Karan Warrier <kawarrier@yahoo.com>
2020-05-25 01:31:08 -0700

    add communication between the UI posting and the backend messages list with EventEmitter, Output, ViewChild

--- frontend/src/app/new-message.component.ts
+++ frontend/src/app/new-message.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core'
+import { Component, Output, EventEmitter } from '@angular/core'
 import {WebService} from './web.service';
 
 @Component({
@@ -21,11 +21,14 @@ import {WebService} from './web.service';
 })
 
 export class NewMessageComponent {
+       @Output() onPosted = new EventEmitter();
+
        constructor(private webService: WebService) {}
 
        message = {owner: "", text: "" }
 
        post() {
-       	      console.log(this.message);
+       	      this.webService.postMessage(this.message);
+	      this.onPosted.emit(this.message);
        }
 }
