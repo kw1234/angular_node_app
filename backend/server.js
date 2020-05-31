645ef3b1884494eb36544408923cb1a46f19472f
Karan Warrier <kawarrier@yahoo.com>
2020-05-31 12:30:42 -0700

    add backend handling of auth route with an additional router, receiving of the http.post on the angular frontend

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,8 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 
-messages = [{text:'lala', owner: 'Mola'}, {text: 'bosa', owner: 'meeshu'}];
+var messages = [{text:'lala', owner: 'Mola'}, {text: 'bosa', owner: 'meeshu'}];
+var users = []
 
 // need this to make sense of the body of requests being Posted
 app.use(bodyParser.json());
@@ -13,7 +14,9 @@ app.use((req, res, next) => {
 	next();
     });
 
+//create separate routers for api and auth so that the prefixes 'api/' and 'auth/' will be handled by different routers
 var api = express.Router();
+var auth = express.Router();
 
 api.get('/messages', (req, res) => {
 	res.send(messages);
@@ -32,6 +35,12 @@ api.post('/messages', (req, res) => {
 	res.json(req.body);
     });
 
+auth.post('/register', (req, res) => {
+	users.push(req.body);
+        res.json(req.body);
+    });
+
 app.use('/api', api);
+app.use('/auth', auth);
 
 app.listen(63145);
